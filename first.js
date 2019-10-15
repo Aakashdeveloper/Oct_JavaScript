4d83c25cf3a08dac43147c788ad5551ce9333397
aakashDeveloper <ahanda205@gmail.com>
2019-10-15 04:22:06 +0100

    a

---
+++ first.js
@@ -0,0 +1,126 @@
+var a = 10
+
+string
+Number
+Boolean
+
+//String
+var a = "aa"
+"1"
+'aa'
+'dfh"
+'true'
+
+//number
+353
+35.4365
+5465475686789679.4636456547
+
+//Boolean
+true / false
+
+var city = "london"
+undefined
+typeof(city)
+"string"
+var marks = 10
+undefined
+typeof(marks)
+"number"
+var marks = '10'
+undefined
+typeof(marks)
+"string"
+var option = true
+undefined
+typeof(option)
+"boolean"
+var option = 'true'
+undefined
+typeof(option)
+"string"
+/////////////////////
+var a = 10
+var b = 10
+a+b
+
+var a = 10
+var b = 10
+a+b
+20
+var a = "Hi "
+undefined
+var b = "John"
+undefined
+a+b
+"Hi John"
+
+number+number = number
+string+number = string
+number+string = string
+string+string = string
+var a = 10.98686
+undefined
+typeof(a)
+"number"
+
+10+10+10 = 30
+10+10+"10" = "2010"
+"10"+10+10 = "101010"
+10+"10"+10 = "101010"
+
+var a = 10
+undefined
+var b = 2
+undefined
+a-b
+8
+a*2
+20
+a/b
+5
+a%b
+0
+10%3
+1
+10%4
+2
+4%10
+4
+3/2
+1.5
+3%2
+1
+2%3
+2
+
+/////////
+var a = "hi "
+undefined
+var b = "John"
+undefined
+a-b
+NaN
+a*b
+NaN
+a/b
+NaN
+a%b
+NaN
+
+true+true = 1
+true+false =1
+false+false =0
+
+true == 1
+false ==  0
+
+"hi"+true+true= "hitruetrue"
+5+true = 
+
+//////////
+truthy
+true <1 
+
+falsy
+false 0 >0 null undefined
---
+++ second.js
@@ -0,0 +1,44 @@
+=     ==> assignment
+==    ==> Compare data only
+===   ==> Compare data and datatype
+
+
+
+var a = 10
+undefined
+var b = 10
+undefined
+var c = '10'
+undefined
+a=b
+10
+a==b
+true
+a==c
+true
+a === b
+true
+a === c
+false
+a == c
+true
+
+console.log("hi")
+alert("hii")
+prompt('What is your name')
+prompt('What is your name')
+"aakash"
+var name = prompt('What is your name')
+undefined
+name
+"aakash"
+confirm("Do you want to leave")
+
+confirm("Do you want to leave")
+true
+confirm("Do you want to leave")
+false
+var a = confirm("Do you want to leave")
+undefined
+a
+true
