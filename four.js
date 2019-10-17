0125b3f35c87038172dae316fe7ad8f76f49fa86
aakashDeveloper <ahanda205@gmail.com>
2019-10-17 04:16:15 +0100

    a

---
+++ four.js
@@ -0,0 +1,82 @@
+10 + 10 = 20
+"10" + "10" "1010"
+
+
+var a = "10"
+undefined
+var b = "10"
+undefined
+a+b
+"1010"
+parseInt(a)+parseInt(b)
+20
+var c = "10.233"
+undefined
+parseInt(a)+parseInt(b)+parseInt(c)
+30
+parseInt(a)+parseInt(b)+parseFloat(c)
+30.233
+
+var x = 10
+undefined
+var y = 20
+undefined
+x>y
+false
+x<y
+true
+x == y
+false
+x !== y
+true
+
+var x = 10
+undefined
+var y = 20
+undefined
+x>y
+false
+x<y
+true
+x == y
+false
+x !== y
+true
+x == 10 ||  y == 10
+true
+x == 10 &&  y == 10
+false
+
+true && true 
+true && true 
+true
+1&1
+1
+1&0
+0
+
+
+var a = true
+!a
+
+var a = true
+a = !a
+
+var a = true
+undefined
+!a
+false
+var b = !a
+undefined
+b
+false
+!b
+true
+var a = 1
+undefined
+!a
+false
+var a = 0
+undefined
+!a
+true
